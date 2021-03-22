db1985f28e52131a4d0a3e711bf10f9d4a0aaed4
gowtham <gowtham25121999s@gmail.com>
2021-03-22 11:04:35 +0530

    adding datatypes file

---
+++ datatypes/Datatypes.java
@@ -0,0 +1,20 @@
+public class Datatypes {
+       public static void main(String args[]) {
+       int a =10;
+       short b =100;
+       float c =70;
+       long d =1000;
+       double e  =1500;
+       boolean f =true;
+       char g  ='m';
+       String s ="gowtham";
+       System.out.println(a);
+       System.out.println(b);
+       System.out.println(c);
+       System.out.println(d);
+       System.out.println(e);
+       System.out.println(g);
+       System.out.println(s);
+       }
+     } 
+       

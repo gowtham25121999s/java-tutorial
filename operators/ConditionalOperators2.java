9c8289143bfa8fbf5bce759d2984bbe92abef588
gowtham <gowtham25121999s@gmail.com>
2021-03-22 11:37:33 +0530

    adding ConditionalOperators2.java

---
+++ operators/ConditionalOperators2.java
@@ -0,0 +1,33 @@
+public class ConditionalOperators2 {
+       public static void main(String arg[]) {
+       int input =75;
+       if(input<50){
+       System.out.println("input is lessthan Fifty");
+       } else {
+       System.out.println("input is not lessthan Fifty");
+       }
+       if(input>=50){ 
+       System.out.println("input is greaterthan or equal to Fifty");
+       } else {
+       System.out.println("input is not greaterthan or equal to Fifty");
+       } 
+       if(input<=50){
+       System.out.println("input is lessthan or equal to Fifty");
+       } else {
+       System.out.println("input is not lessthan or equal to Fifty");
+       }
+       if(input==50){
+       System.out.println("input is equal to Fifty");
+       } else {
+       System.out.println("input is not equal to Fifty");
+       } 
+       if(input!=50){
+       System.out.println("input is not equal to Fifty");
+       } else {
+       System.out.println("input is equal to Fifty");
+       }
+      }
+     }
+     
+       
+       

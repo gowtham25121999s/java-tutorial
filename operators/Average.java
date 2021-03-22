8ed6912a04d35777512441fef10a98ee9fe29194
gowtham <gowtham25121999s@gmail.com>
2021-03-22 11:31:18 +0530

    adding Average.java file

---
+++ operators/Average.java
@@ -0,0 +1,12 @@
+public class Average {
+       public static void main(String args[]) {
+       int tamilMark =82;
+       int englishMark =85;
+       int mathsMark =80;
+       int scienceMark =99;
+       int socialscienceMark  =95;
+       int totalMark =tamilMark+englishMark+mathsMark+scienceMark+socialscienceMark;
+       float average =totalMark/5.0f;
+       System.out.println(average);
+       }
+     } 

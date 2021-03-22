e98a4c655814d56c212cfc81c82e1340bf34f9a4
gowtham <gowtham25121999s@gmail.com>
2021-03-22 11:40:36 +0530

    adding Operators.java file

---
+++ operators/Operators.java
@@ -0,0 +1,40 @@
+public class Operators {
+      public static void main(String args[]) {
+      int a =10;
+      int b =60;
+      int c =a+b;
+      a =50;
+      b =30;
+      int f =a-b;
+      int g =80;
+      int h =20;
+      int i =g*h;
+      int j =80;
+      int k =20;
+      int l =j/k;
+      int m =70;
+      int n =15;
+      int o =m%n;
+      int p =10;
+      int q =p++;
+      int r =20;
+      int s =++r;
+      int t =40;
+      int u =t--;
+      int v =50;
+      int w =--v;
+      System.out.println(c);
+      System.out.println(f);
+      System.out.println(i);
+      System.out.println(l);
+      System.out.println(o);
+      System.out.println(p);
+      System.out.println(q);
+      System.out.println(r);
+      System.out.println(s);
+      System.out.println(t);
+      System.out.println(u);
+      System.out.println(v);
+      System.out.println(w);
+      }
+    }

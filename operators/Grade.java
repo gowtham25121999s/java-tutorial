a5dd0af761f6817111c104b45038a9a707065397
gowtham <gowtham25121999s@gmail.com>
2021-03-22 12:32:23 +0530

    adding Grade.java file

---
+++ operators/Grade.java
@@ -0,0 +1,17 @@
+public class Grade {
+       public static void main(String arg[]) { 
+       int a,b,c;
+       a=26;
+       b=39;
+       c=8;
+       if(a>b&&a>c) {
+       System.out.println("a");
+       }
+       if(b>a&&b>c) {
+       System.out.println("b");
+       } 
+       if(c>a&&c>b) {
+       System.out.println("c");
+       } 
+      } 
+     }

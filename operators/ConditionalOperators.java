33b0fa609cde37a46478ea08c18050615210998a
gowtham <gowtham25121999s@gmail.com>
2021-03-22 11:35:17 +0530

    adding ConditionalOperators.java file

---
+++ operators/ConditionalOperators.java
@@ -0,0 +1,11 @@
+public class ConditionalOperators{
+        public static void main(String args[]) {
+        int average =40;
+        boolean aboveFifty =average>50;
+        if(aboveFifty) {
+        System.out.println("Good");
+        } else {
+        System.out.println("Bad");
+        } 
+       } 
+      }
